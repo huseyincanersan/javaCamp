d11df82c27013e9fb2831376a52ed36242ee9962
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-04-06 02:00:12 +0300

    oopIntro projesi güncellendi

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -4,49 +4,58 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Product product1 = new Product(1,"Lenovo V14", 15000,"16 GB ram"); // Ornek olusturma, intance
-		Product product2 = new Product(2,"Lenovo V15",10000,"8 gb ram"); 
-		Product product3 = new Product(3,"HP 5",20000,"32 GB RAM");
- 
-		
+		Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB ram", 10); // Ornek olusturma, intance
+		Product product2 = new Product();
+		Product product3 = new Product();
+
+		product2.setId(2);
+		product2.setName("Lenovo V15");
+		product2.setDetail("16 GB Ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(16000);
+
+		product3.setId(2);
+		product3.setName("Hp 5");
+		product3.setDetail("8 GB Ram");
+		product3.setDiscount(10);
+		product3.setUnitPrice(10000);
+
 		Product[] products = { product1, product2, product3 };
 
-		// System.out.println(products.length);
-
 		for (Product product : products) {
-			System.out.println(product.id);
-			System.out.println(product.name);
-			System.out.println(product.unitPrice);
-			System.out.println(product.detail);
+			System.out.println(product.getId());
+			System.out.println(product.getName());
+			System.out.println(product.getUnitPrice());
+			System.out.println(product.getDetail());
+			System.out.println(product.getDiscount());
+			System.out.println(product.getUnitPriceAfterDiscount());
 			System.out.println("---------");
 		}
-		
+
 		Category category1 = new Category();
 		Category category2 = new Category();
+
+		category1.setId(395);
+		category1.setName("Pc");
 		
-		category1.id=385;
-		category1.name="Pc";
+		category2.setId(396);
+		category2.setName("Kýrtasiye");
 		
-		
-		category2.id=386;
-		category2.name="Home";
-		
-		Category[] categorys = {category1,category2};
-		
+		Category[] categorys = { category1, category2 };
+
 		for (Category category : categorys) {
-			System.out.println(category.id);
-			System.out.println(category.name);
+			System.out.println(category.getId());
+			System.out.println(category.getName());
 			System.out.println("-------------");
 		}
-		
+
 		ProductManager productmanager = new ProductManager();
-		
+
 		productmanager.addToCart(product1);
-		
+
 		productmanager.addToCart(product2);
-		
+
 		productmanager.addToCart(product3);
-		
 
 	}
 
